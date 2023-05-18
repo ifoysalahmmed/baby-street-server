ea612f1ca1a53552b534b5d2a68d326a9256e5d5
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-18 16:58:15 +0600

    added mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,3 +16,31 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Baby Street Server is Running on Port: ${port}`);
 });
+
+const uri =
+  "mongodb+srv://babyStreet:rL1olaV3vhj17ESr@cluster0.llwcx8n.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
