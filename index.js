b664ded659b1297ad4faf39f44978b78168e5b05
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-20 01:06:44 +0600

    added limit in allToys api

--- index.js
+++ index.js
@@ -49,8 +49,19 @@ async function run() {
 
     // reading toys info
     app.get("/allToys", async (req, res) => {
-      const result = await toyCollection.find().sort({ name: 1 }).toArray();
-      res.send(result);
+      const limit = parseInt(req.query.limit);
+      if (limit === 20) {
+        const result = await toyCollection
+          .find()
+          .sort({ name: 1 })
+          .limit(limit)
+          .toArray();
+
+        res.send(result);
+      } else {
+        const result = await toyCollection.find().sort({ name: 1 }).toArray();
+        res.send(result);
+      }
     });
 
     // reading category wise toys info
